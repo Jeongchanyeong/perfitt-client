db1fce5897d9d0759970c88222083fbd43bc7a1f
suziebelle003 <imsubin003@gmail.com>
2024-09-06 12:21:58 +0900

    feat: signin social btn #11

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,6 +1,7 @@
 import '../css/fonts.css';
 import '../css/tailwind.css';
-
+import kakakologo from '../assets/images/kakaologo.png';
+import naverlogo from '../assets/images/naverlogo.png'
 
 const SignIn = () => {
   return (
@@ -8,11 +9,11 @@ const SignIn = () => {
     <div className="w-[200px] h-[190px]" id="social_sign-in_buttons">
       <div className="flex flex-col p-[8px] gap-[6px] text-[14px]" id="social_button_wrapper">
         <button className="flex items-center justify-center w-[184px] h-[32px] rounded-[4px] bg-[#F5F5F5]">
-          <img className="w-[24px] h-[24px] rounded-lg mr-[4px]" src="../../public/naverlogo.png" alt='naverlogo' />
+          <img className="w-[24px] h-[24px] rounded-lg mr-[4px]" src={naverlogo} alt='naverlogo' />
           네이버
         </button>
         <button className="flex items-center justify-center  w-[184px] h-[32px] rounded-[4px] bg-[#F5F5F5]">
-            <img className="w-[24px] h-[24px] rounded-lg mr-[4px]" src="../../public/kakaologo.png" alt='kakaologo' />
+            <img className="w-[24px] h-[24px] rounded-lg mr-[4px]" src={kakakologo} alt='kakaologo' />
             카카오
             </button>
         <button className="w-[184px] h-[32px] rounded-[4px] bg-[#F5F5F5]">이메일 로그인</button>
