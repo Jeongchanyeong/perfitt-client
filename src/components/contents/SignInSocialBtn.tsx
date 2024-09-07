ec12615b5f49adf7cbd508630bf32550328a5954
suziebelle003 <imsubin003@gmail.com>
2024-09-07 16:14:53 +0900

    feat:SignInSocialBtn 수정#11

--- src/components/contents/SignInSocialBtn.tsx
+++ src/components/contents/SignInSocialBtn.tsx
@@ -8,18 +8,18 @@ const SignInSocialBtn = () => {
   return (
     <>
     <div className="w-[200px] h-[190px]" id="social_sign-in_buttons">
-      <div className="flex flex-col p-[8px] gap-[6px] text-[14px]" id="social_button_wrapper">
-        <button onClick={onClickSociallogin} className="flex items-center justify-center w-[184px] h-[32px] rounded-[4px] bg-[#F5F5F5]">
-          <img className="w-[24px] h-[24px] rounded-lg mr-[4px]" src={naverlogo} alt='naverlogo' />
+      <div className="flex flex-col p-[8px] gap-[6px] text-[14px] leading-[22px] font-normal" id="social_button_wrapper">
+        <button onClick={onClickSociallogin} className="flex items-center justify-center w-[184px] h-[32px] rounded-[4px] bg-[#F5F5F5] ">
+          <img className="w-[24px] h-[24px] rounded-lg mr-[8px]" src={naverlogo} alt='naverlogo' />
           네이버
         </button>
-        <button onClick={onClickSociallogin} className="flex items-center justify-center  w-[184px] h-[32px] rounded-[4px] bg-[#F5F5F5]">
-            <img className="w-[24px] h-[24px] rounded-lg mr-[4px]" src={kakakologo} alt='kakaologo' />
+        <button onClick={onClickSociallogin} className=" flex items-center justify-center  w-[184px] h-[32px] rounded-[4px] bg-[#F5F5F5]">
+            <img className="w-[24px] h-[24px] rounded-lg mr-[8px]" src={kakakologo} alt='kakaologo' />
             카카오
             </button>
-        <button onClick={onClickSociallogin} className="w-[184px] h-[32px] rounded-[4px] bg-[#F5F5F5]">이메일 로그인</button>
+        <button onClick={onClickSociallogin} className=" w-[184px] h-[32px] rounded-[4px] bg-[#F5F5F5]">이메일 로그인</button>
         <span className="text-center w-[184px] h-[22px]">또는</span>
-        <button onClick={onClickSociallogin} className="w-[184px] h-[32px] rounded-[4px] bg-[#F5F5F5]">회원가입 하기</button>
+        <button onClick={onClickSociallogin} className=" w-[184px] h-[32px] rounded-[4px] bg-[#F5F5F5]">회원가입 하기</button>
       </div>
     </div>
     </>
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,11 +1,10 @@
-import SignInAuthList from "../components/contents/SignInAuthList";
-import SignInAutoKeyword from "./SignInAutoKeyword";
+
 
 
 const SignIn = () => {
   return (
     <>
-    <SignInAutoKeyword/>
+    
     </>
   );
 }
