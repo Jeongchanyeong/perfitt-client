7ed510408420da05e2a93a8eec0913b920feb80f
suziebelle003 <imsubin003@gmail.com>
2024-09-29 21:29:28 +0900

    feat: zustand user정보 upload

---
+++ src/stores/UserState.tsx
@@ -0,0 +1,13 @@
+import create from 'zustand';
+
+// Zustand 상태 정의
+type UserState = {
+  user: TUser | null; // 사용자 정보
+  setUser: (user: TUser | null) => void; // 사용자 정보를 설정하는 함수
+};
+
+// Zustand로 상태 생성
+export const useUserStore = create<UserState>(set => ({
+  user: null,
+  setUser: user => set({ user }),
+}));
